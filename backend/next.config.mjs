c9a65823fdf13e8de816c66f8d10df8ccc9dc77f
Joseba <joseba@debian.localhost>
2025-12-12 13:21:18 +0100

    issues

--- backend/next.config.mjs
+++ backend/next.config.mjs
@@ -2,6 +2,9 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
